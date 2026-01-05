776d3fd98b76252b8161580f5d8e2be9c3f2d1be
xuantrungtrinh <yuukiritokunzz@gmail.com>
2026-01-05 12:53:39 +0700

    Update gitignore and backend config

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -29,17 +29,3 @@ def create_app():
     app.register_blueprint(vocab_bp)
 
     return app
-
-# from flask import Flask
-# from .extensions import db
-#
-# def create_app():
-#     app = Flask(__name__)
-#     app.config.from_object("config.Config")
-#
-#     db.init_app(app)
-#
-#     from ..app.routes.vocab import vocab_bp
-#     app.register_blueprint(vocab_bp)
-#
-#     return app
--- backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = "dev-secret-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/database.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
